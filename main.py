a1b124562f7291d6f95bd1df7ebb5cc765f8a799
Riley Thompson <100237088+riltho1@users.noreply.github.com>
2023-03-12 21:03:00 +0000

    I added an enemy class which does not work at the moment and now I have a few errors to fix involving the class.

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import discord
 import random
 from discord.ext import commands
 
-TOKEN = os.environ['TOKEN']
-
+TOKEN = os.environ['TOKEN']#Importing token for bot to run script
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix = "!", intents = intents)
 
-class rng():
+user_hp = 20
+basic_attack = 10 
+
+@client.event
+async def on_ready():
+  print(f"{client.user} has connected to discord")
+
+class Stats:
   def __init__(self,name,hp,max_hp,attack,defense): #Defining variables
     self.name = name
     self.hp = hp
@@ -27,7 +33,12 @@ class rng():
       damage = 0
 
     other.hp -= damage
-
     return (self.attack, other.hp <= 0) #Damage, fatal
+
+  class Enemy(Stats):
+    def __init__(self,name,hp,max_hp,attack,defense):
+      super().__init__(self,name,hp,max_hp,attack,defense)
+      
+    
 
 client.run(TOKEN)
