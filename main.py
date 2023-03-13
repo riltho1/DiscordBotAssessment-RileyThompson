461458c63fa97d9e9c019150860521cdc2eef2e4
Riley Thompson <100237088+riltho1@users.noreply.github.com>
2023-03-13 06:38:20 +0000

    I have gotten rid of the fight function because it seems a lot more complex than what I actually needed. I have instead added user_attack which is going to be rng based attack damage when it is done.

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix = "!", intents = intents)
 
-user_hp = 20
-basic_attack = 10 
-heal = 10
+name = "Bob"
+hp = 20
+max_hp = hp + 15
+attack = 10 
+defense = 15
+
+
 
 @client.event
 async def on_ready():
   print(f"{client.user} has connected to discord")
 
-class Stats:
+#This was copied from previous template and wasn't needed
+"""
+class rng:
+  def __init__(self):
+    self.secret_number = random.randint(0,10)
+
+  def reset(self):
+    self.secret_number = random.randint(0,10)
+"""
+
+class Stats():
   def __init__(self,name,hp,max_hp,attack,defense): #Defining variables
     self.name = name
     self.hp = hp
@@ -25,6 +39,12 @@ class Stats:
     self.attack = attack
     self.defense = defense
 
+  def user_attack(self,name,hp,max_hp,attack,defense):
+    damage = random.randint(1,10)
+    print(damage)
+
+#This seems a bit complex and unnessasary so I have decided to make it simpler above
+'''
   def fight(self,other):
     defense = min(other.defense, 19) #Gives maximum defense cap
     hit_chance = random.randint(0,20 - defense)
@@ -35,7 +55,7 @@ class Stats:
 
     other.hp -= damage
     return (self.attack, other.hp <= 0) #Damage, fatal
-
+'''
 class Character(Stats):# Users stats
   def __init__(self, name, hp, max_hp, attack, defense):
     super().__init__(name, hp, max_hp, attack, defense)
@@ -49,5 +69,6 @@ class Zombie(Enemy):
   def __init__(self):
     super().__init__("Zombie",20,5,7)
   
+
 
 client.run(TOKEN)
