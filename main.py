31bedab5d648ba3d6f66d4ceef4e31a4a74b80cb
Riley Thompson <100237088+riltho1@users.noreply.github.com>
2023-03-15 07:48:58 +0000

    I restarted because it was getting quite messy. This time I used two separate classes for the user and the enemy in order to stop confusing myself and assigned my variable as before. I then returned these variables to functions so I could use them in commands. I also assigned values to init for each class and began working on commands

--- main.py
+++ main.py
@@ -9,99 +9,83 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix = "!", intents = intents)
 
-name = "Bob"
-hp = 20
-max_hp = hp + 15
-attack = 10 
-defense = 15
-
-
-
 @client.event
 async def on_ready():
   print(f"{client.user} has connected to discord")
 
-#This was copied from previous template and wasn't needed
-"""
-class rng:
-  def __init__(self):
-    self.secret_number = random.randint(0,10)
-
-  def reset(self):
-    self.secret_number = random.randint(0,10)
-"""
-
-class Stats():
-  #Defining variables
-  def __init__(self,name,hp,max_hp,attack,defense): 
-    self.name = name
-    self.hp = hp
-    self.max_hp = max_hp
-    self.attack = attack
-    self.defense = defense
+#User class containing player stats
+class Player:
+  def __init__(self, P_attack, P_s_attack, P_hp, P_max_hp, P_heal, P_Name):
     
-  def user_attack(self): #Only needed self 
-    damage = random.randint(1,10)
-    print(damage)
+#Giving variables for stats
+    self.attack = P_attack
+    self.s_attack= P_s_attack
+    self.hp = P_hp
+    self.max_hp = P_max_hp
+    self.heal = P_heal
+    self.name = P_Name
 
-@client.command("attack")
-async def attack(ctx):
+  #Assinging the variables to User functions
+  def get_attack(self):
+    return self.attack
 
-  def check_valid(m):
-    return ctx.author== m.author and m.content == "attack"
+  def get_hp(self):
+    return self.hp
 
-  
-  while True:
-    await ctx.channel.send("You attack for")
-    await ctx.channel.send("The monster attacks")
-    msg = await client.wait_for("message", check = check_valid)
+  def get_max_hp(self):
+    return self.max_hp
 
-#This seems a bit complex and unnessasary so I have decided to make it simpler above
-'''
-  def fight(self,other):
-    defense = min(other.defense, 19) #Gives maximum defense cap
-    hit_chance = random.randint(0,20 - defense)
-    if hit_chance:
-      damage = self.attack
-    else:
-      damage = 0
+  def get_heal(self):
+    return self.heal
 
-    other.hp -= damage
-    return (self.attack, other.hp <= 0) #Damage, fatal
-'''
-class Character(Stats):# Users stats
-  def __init__(self, name, hp, max_hp, attack, defense):
-    super().__init__(name, hp, max_hp, attack, defense)
+  def get_name(self):
+    return self.name
 
-class Enemy(Stats):#Enemy stats
-    def __init__(self, name, hp, max_hp, attack, defense):
-      super().__init__(name, hp, max_hp, attack, defense)
+user = Player(20, 40, 75, 100, 15, "Phillip")
 
-#Enemies
-class Zombie(Enemy):
-  def __init__(self):
-    super().__init__("Zombie",20,25,7,10)
-  
+#Enemy class containing enemy stats
+class Enemy:
+  def __init__(self, E_attack, E_s_attack, E_hp, E_max_hp, E_heal, E_Name):
+#Giving variables for stats
+    self.attack = E_attack
+    self.s_attack = E_s_attack
+    self.hp = E_hp
+    self.max_hp = E_max_hp
+    self.heal = E_heal
+    self.name = E_Name
 
+#Asigning the variables to Enemy functions
+  def get_attack(self):
+    return self.attack
+
+  def get_hp(self):
+    return self.hp
+
+  def get_max_hp(self):
+    return self.max_hp
+
+  def get_heal(self):
+    return self.heal
+
+  def get_name(self):
+    return self.name
+    
+enemy = Enemy(15, 50, 75, 90, 15, "Zombie")
+
+#Commands
+#In progress
 @client.command(name = "start")
 async def start(ctx):
-  await ctx.channel.send("A zombie has appeared")
+  await ctx.channel.send
 
-#This shows the value of damage for each Character and enemy when the user inputs !stats it shows up in order on the console
 @client.command (name = "stats")
 async def stats(ctx):
-  player = Character("Bob", 20, 35, 10, 15)
-  zombie = Zombie()
-  player.user_attack()
-  zombie.user_attack()
-#Trying to test the user_attack function (errors)
-'''
-super(Zombie).user_attack("Zombie",20,25,7,10)
-s = Stats("Zombie",20,25,7,10)
+  await ctx.channel.send(f"{user.get_name()} has {user.get_hp()} hp out of {user.get_max_hp()} hp")
+  await ctx.channel.send(f"{user.get_name()} has an attack force of {user.get_attack()}")
 
-@client.command (name = "stats")
-async def stats(ctx,str):
-  if str(str) == s.user_attack:
-    await ctx.channel.send(Stats)
-'''
+#In progress
+@client.command (name = "attack")
+async def attack(ctx):
+  
+
 client.run(TOKEN)
